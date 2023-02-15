41b38da78bbe5dadc58c7b8191084efcb363932c
AnsonCar <anson1tsang@gmail.com>
2023-02-15 18:33:30 +0800

    ip

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
-const http = require('http');
-const fs = require('fs');
+var http = require("http");
 
-const server = http.createServer((req, res) => {
-  fs.readFile('/root/TonyaPuzzle/index.html', (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.write('File not found!');
-    } else {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-    }
-    res.end();
-  });
-});
-
-server.listen(3000, () => {
-  console.log('Server running at http://127.0.0.1:3000/');
-});
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World, I am 3000");
+  response.end();
+}).listen(3000);
